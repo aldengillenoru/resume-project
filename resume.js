60ee21e0b73ffc5e8f99636ac44ccc40dd2a27bf
aldengillenoru <agil4081@oru.edu>
2025-09-17 09:17:34 -0500

    resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+// Define variables
+let myName = "Alden";
+let title = "Student";
+let experience = "Broken Arrow Roller Sports, Rainbow Bowls, Marble Slab Creamery, Lawn Mowing";
+let education = "Oral Roberts University";
+let skills = ["photoshop", "mowing", "cryptocurrency", "working with children", "customer service"];
+//let skills = "something"; //TODO
+
+// Connect variables to the HTML ligaments
+document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
